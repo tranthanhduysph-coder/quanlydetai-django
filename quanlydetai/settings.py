b7fbb62856ddfdc67a0365ab6a2ebeca334baa8d
Tran Thanh Duy <email-github-cua-ban@gmail.com>
2025-11-08 23:16:04 +0700

    Prepare code for Render deployment

--- quanlydetai/settings.py
+++ quanlydetai/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 import os
 from dotenv import load_dotenv
-
+import dj_database_url
 load_dotenv() # Tải các biến từ tệp .env
 from pathlib import Path
 
@@ -81,10 +81,10 @@ WSGI_APPLICATION = 'quanlydetai.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=f'sqlite:///{BASE_DIR / "db.sqlite3"}',
+        conn_max_age=600
+    )
 }
 
 
